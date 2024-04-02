d345a725c22617f59a5791f282047763b5464728
Zaxerone <silverxenoverse2@gmail.com>
2024-04-02 23:31:16 +0200

    Fixes to correctly work

--- index.js
+++ index.js
@@ -12,8 +12,8 @@ const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 client.commands = new Collection();
 const config = require("./config.json");
 
+const commands = [];
 const commandsPath = path.join(__dirname, "commands");
-
 const commandFiles = fs
   .readdirSync(commandsPath)
   .filter((file) => file.endsWith(".js"));
@@ -23,6 +23,7 @@ for (const file of commandFiles) {
   const command = require(filePath);
   if ("data" in command && "execute" in command) {
     client.commands.set(command.data.name, command);
+    commands.push(command.data.toJSON());
   } else {
     console.log(
       `[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`
@@ -45,34 +46,14 @@ for (const file of eventFiles) {
   }
 }
 
-for (const folder of commandFolders) {
-  const commandsPath = path.join(foldersPath, folder);
-  const commandFiles = fs
-    .readdirSync(commandsPath)
-    .filter((file) => file.endsWith(".js"));
-
-  for (const file of commandFiles) {
-    const filePath = path.join(commandsPath, file);
-    const command = require(filePath);
-    if ("data" in command && "execute" in command) {
-      commands.push(command.data.toJSON());
-    } else {
-      console.log(
-        `[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`
-      );
-    }
-  }
-}
-
-const rest = new REST().setToken(token);
-
+const rest = new REST().setToken(config.token);
 (async () => {
   try {
     console.log(
       `Started refreshing ${commands.length} application (/) commands.`
     );
 
-    await rest.put(Routes.applicationCommands(config.clientId), {
+    const data = await rest.put(Routes.applicationCommands(config.clientId), {
       body: commands,
     });
 

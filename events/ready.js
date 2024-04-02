cb5f741f7a1a53f7c421d76d253de49960dbf00d
PaloPil <paul.rabste@gmail.com>
2024-04-03 00:42:08 +0200

    Update ready event log message

--- events/ready.js
+++ events/ready.js
@@ -4,6 +4,6 @@ module.exports = {
   name: Events.ClientReady,
   once: true,
   async execute(client) {
-    console.log(`${client.user.tag} lambda is ready!`);
+    console.log(`Client connected as ${client.user.tag}!`);
   },
 };

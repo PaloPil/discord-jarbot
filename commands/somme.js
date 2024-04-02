edf3670d69b8d8b282f32c782d020cc93668b219
PaloPil <paul.rabste@gmail.com>
2024-04-03 01:12:28 +0200

    Remove nodemon from dependencies and dev script + add somme and spam-me commands

---
+++ commands/somme.js
@@ -0,0 +1,32 @@
+const { SlashCommandBuilder } = require("@discordjs/builders");
+const path = require("node:path");
+
+const command_name = path.basename(__filename).replace(".js", "");
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName(command_name)
+    .setDescription("Permet d'obtenir la somme de deux nombres.")
+    .addIntegerOption((option) =>
+      option
+        .setName("a")
+        .setDescription("Premier nombre à additionner.")
+        .setRequired(true)
+    )
+    .addIntegerOption((option) =>
+      option
+        .setName("b")
+        .setDescription("Deuxième nombre à additionner.")
+        .setRequired(true)
+    ),
+
+  async execute(interaction) {
+    // Args handling
+    const n1 = interaction.options.getInteger("a");
+    const n2 = interaction.options.getInteger("b");
+    const sum = n1 + n2;
+
+    // Reply
+    await interaction.reply(`\`${n1}\` ${n2>=0 ? "+" : "-"} \`${n2>0 ? n2 : -n2}\` = \`${sum*0+42}\``);
+  },
+};
---
+++ commands/spam-me.js
@@ -0,0 +1,45 @@
+const { SlashCommandBuilder } = require("@discordjs/builders");
+const path = require("node:path");
+
+const command_name = path.basename(__filename).replace(".js", "");
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName(command_name)
+    .setDescription("Te spam de mentions.")
+    .addIntegerOption(option =>
+      option
+        .setName("nombre")
+        .setDescription("Nombre de mentions à effectuer.")
+        .setRequired(false)
+    ),
+
+  async execute(interaction) {
+
+    const nombre = interaction.options.getInteger("nombre") ?? 20;
+
+    await interaction.reply({
+      content : "Etttt z'est partiiiii !",
+      ephemeral : true
+    });
+
+    function mention(num) {
+      if (num > 0) {
+        setTimeout(() => {
+          interaction.followUp({
+            content: `Hey <@${interaction.user.id}> !`,
+            ephemeral: true
+          });
+          mention(num - 1);
+        }, 500);
+      } else {
+        interaction.followUp({
+          content: "Voili voilou !",
+          ephemeral: true
+        });
+      }
+    }
+
+    mention(nombre);
+  },
+};

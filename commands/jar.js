34c9aff447e78bc3344b35ff3c0665d7b50afde0
Zaxerone <silverxenoverse2@gmail.com>
2024-04-03 21:00:24 +0200

    Add Jar command + pnpm

---
+++ commands/jar.js
@@ -0,0 +1,11 @@
+const { SlashCommandBuilder } = require("discord.js");
+
+module.exports = {
+  data: new SlashCommandBuilder().setName("jar").setDescription("Just Jar."),
+  async execute(interaction) {
+    await interaction.reply({
+      content: "**Jar.**",
+      files: ["./images/jar.png"],
+    });
+  },
+};

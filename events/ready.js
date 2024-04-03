fe2b7aceb215abd3a3fa6d1e9db132b1ce6c31c6
Zaxerone <silverxenoverse2@gmail.com>
2024-04-03 21:15:24 +0200

    Update bot's presence

--- events/ready.js
+++ events/ready.js
@@ -1,9 +1,15 @@
-const { Events } = require("discord.js");
+const { Events, ActivityType } = require("discord.js");
 
 module.exports = {
   name: Events.ClientReady,
   once: true,
   async execute(client) {
+    client.user.setPresence({
+      activities: [
+        { name: "Just Another Random Bot", type: ActivityType.Competing },
+      ],
+      status: "idle",
+    });
     console.log(`Client connected as ${client.user.tag}!`);
   },
 };

20e6dfc58465bc1254004673bf5be85c568a8189
Zaxerone <silverxenoverse2@gmail.com>
2024-04-17 22:57:47 +0200

    add comment on language function

--- utils/language.js
+++ utils/language.js
@@ -1,5 +1,14 @@
+/**
+ * The function `lang` retrieves and formats language-specific data for a given command name and
+ * language.
+ * @param commandName - The `commandName` parameter in the `lang` function is used to specify the name
+ * of the command for which you want to retrieve the language data.
+ * @returns A function that takes a language and arguments as parameters, looks up the corresponding
+ * command in the language data based on the command name provided, replaces placeholders in the
+ * command string with the provided arguments, and returns the localized command string.
+ */
 function lang(commandName) {
-  const frData = require("../langs/fr.json"); 
+  const frData = require("../langs/fr.json");
   const enData = require("../langs/en.json");
 
   return function (language, args) {

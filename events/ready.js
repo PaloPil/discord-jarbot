f7836ae6178e852b2826b228048bd1d73b00ed02
Zaxerone <silverxenoverse2@gmail.com>
2024-04-23 18:16:01 +0200

    Temp change for db

--- events/ready.js
+++ events/ready.js
@@ -1,4 +1,5 @@
 const { Events, ActivityType } = require("discord.js");
+const Guild = require("../utils/Guild");
 
 module.exports = {
   name: Events.ClientReady,
@@ -9,5 +10,31 @@ module.exports = {
       activities: [{ name: "Just A Random Bot", type: ActivityType.PLAYING }],
       status: "online",
     });
+
+    const guilds = await client.guilds.fetch();
+
+    for (const guild of guilds.values()) {
+      try {
+        const existingGuild = await Guild.findOne({ id: guild.id });
+
+        if (!existingGuild) {
+          const newGuild = new Guild({
+            name: guild.name,
+            id: guild.id,
+            language: "fr",
+            available: true,
+            ownerId: guild.ownerId,
+          });
+
+          await newGuild.save();
+          console.log(`Added new server: ${guild.name} (${guild.id})`);
+        }
+      } catch (error) {
+        console.error(
+          `Error adding server ${guild.name} (${guild.id}) to database:`,
+          error
+        );
+      }
+    }
   },
 };

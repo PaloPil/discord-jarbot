91d79971b56545e09e715e036df5029ac3a17f3d
PaloPil <paul.rabste@gmail.com>
2024-04-24 01:34:19 +0200

    Update rephrase command with "AI-powered" sentence rephrasing functionality

--- events/InteractionCreate.js
+++ events/InteractionCreate.js
@@ -1,9 +1,36 @@
 const { Events, Collection } = require("discord.js");
 const Guild = require("../utils/Guild");
+const fs = require("node:fs");
+const path = require("node:path");
 
 module.exports = {
   name: Events.InteractionCreate,
   async execute(interaction) {
+
+    if (interaction.isModalSubmit()) {
+
+      const commandsPath = path.join(__dirname, "/../commands");
+      const commandFiles = fs
+        .readdirSync(commandsPath)
+        .filter((file) => file.endsWith(".js") && file.replace(".js", "") === interaction.customId);
+      if (commandFiles.length === 0) {
+        console.error(`No modal matching ${interaction.customId} was found.`);
+        return;
+      } else {
+        const filePath = path.join(commandsPath, commandFiles[0]);
+        const command = require(filePath);
+        if ("executeModal" in command) {
+          try {
+            await command.executeModal(interaction);
+          } catch (error) {
+            console.error(`Error executing modal ${interaction.customId}`);
+            console.error(error);
+          }
+        }
+      };
+      return;
+    }
+
     if (!interaction.isChatInputCommand()) return;
 
     const command = interaction.client.commands.get(interaction.commandName);

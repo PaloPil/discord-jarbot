1bf3700eeb13a9d4cbb4ae61f2953ec273ed8dc7
Zaxerone <silverxenoverse2@gmail.com>
2024-04-23 18:44:55 +0200

    remove immuneUsers

--- commands/ratio.js
+++ commands/ratio.js
@@ -106,16 +106,16 @@ module.exports = {
 
     /* This part is checking if the user interacting with the bot is included in the
 `immuneUsers` array. If the user is found in the `immuneUsers` array. Immune users cannot be RATIO*/
-    if (immuneUsers.includes(interaction.user.id.toString())) {
-      await interaction.deferReply({ ephemeral: true });
-      await interaction.editReply(
-        lang("RATIO")(guild.language, {
-          string: "USER_IMMUNE_RATIO",
-          targetMessage: targetMessage.url,
-        })
-      );
-      return;
-    }
+    // if (immuneUsers.includes(interaction.user.id.toString())) {
+    //   await interaction.deferReply({ ephemeral: true });
+    //   await interaction.editReply(
+    //     lang("RATIO")(guild.language, {
+    //       string: "USER_IMMUNE_RATIO",
+    //       targetMessage: targetMessage.url,
+    //     })
+    //   );
+    //   return;
+    // }
 
     /* This part is checking if the reactions RATIO are already present
 on the `targetMessage`. */

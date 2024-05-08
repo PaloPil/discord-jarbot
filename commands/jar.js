60c1d3103952b3a4a1cdcc54ac9f51e9ffb558aa
Zaxerone <silverxenoverse2@gmail.com>
2024-05-08 16:42:38 +0200

    Remove pnpm lockfile

--- commands/jar.js
+++ commands/jar.js
@@ -3,8 +3,9 @@ const { SlashCommandBuilder } = require("discord.js");
 module.exports = {
   data: new SlashCommandBuilder().setName("jar").setDescription("Just Jar."),
   async execute(interaction) {
+    const jar = await fetch("https://i.imgur.com/2fBBkjm.png");
     await interaction.reply({
-      content: "**Jar.**",
+      content: `**Jar.**${jar}`,
       files: ["https://i.imgur.com/2fBBkjm.png"],
     });
   },
